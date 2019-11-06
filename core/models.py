55cdd59b8fef3ffc038a57cb4adf6c84b148d114
Chougrad <l.chougrad@reply.it>
2019-11-06 17:44:46 +0100

    added encrypt and decrypt methods

--- core/models.py
+++ core/models.py
@@ -1,13 +1,56 @@
 from django.db import models
 
+
 # Create your models here.
-class Musician(models.Model):
+class Manager(models.Model):
+    email = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
+class Agency(models.Model):
+    name = models.CharField(max_length=50)
+    p_iva = models.CharField(max_length=50)
+    manager = models.ForeignKey(Manager, null=True, on_delete=models.SET_NULL)
+
+
+
+
+class Employee(models.Model):
+    email = models.CharField(max_length=50)
+    manager = models.ForeignKey(Manager, null=True, on_delete=models.SET_NULL, related_name='employees')
     name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+    last_name = models.CharField(max_length=100)
+
+
+
+class Tought(models.Model):
+    HAPPY = 'HP'
+    SAD = 'SD'
+    ANGRY = 'NG'
+    IN_GOOD_MOOD = 'GM'
+    AWFUL = 'AW'
+    INSPIRED = 'SP'
+    MOODS = (
+        (HAPPY, 'Happy'),
+        (SAD, 'Sad'),
+        (ANGRY, 'Angry'),
+        (IN_GOOD_MOOD, 'In good mood'),
+        (AWFUL, 'Awful'),
+        (INSPIRED, 'Inspired'),
+    )
+ 
+
+
+    mood = models.CharField(
+                max_length=2,
+                choices=MOODS,
+                default=SAD,
+            )
+    employee = models.ForeignKey(Employee, null=True, on_delete=models.DO_NOTHING, related_name='toughts')
+    text = models.CharField(max_length=100)
+    when = models.DateField()
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,102 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+
+
+MY_SECRET_FOR_EVER = 'Sara Cannella' # This is input in the form of a string
+password = MY_SECRET_FOR_EVER.encode() # Convert to type bytes
+salt = b'salt_' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf oncefrom cryptography.fernet import Fernet
+
+
+
+
 
 # Create your views here.
+def engine(request):
+    return JsonResponse({
+     's':  decrypt('gAAAAABdwvhLbiRWb971EFxlEosly7sqV5y14ifHsjEL3u0TvTz1nmGt2d5RoRTG04AGIXfMH8iSQqd201tIgfx67hXcGyUZYw==')
+    })
+
+
+
+
+def decrypt(plain): return Fernet(key).decrypt(plain.encode()).decode('utf-8')
+
+def encrypt(plain): return Fernet(key).encrypt(plain.encode()).decode('utf-8')
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+BS = 8
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+unpad = lambda s : s[0:-ord(s[-1])]
+
+def encode(phrase):
+    key = MY_SECRET_FOR_EVER
+    c1  = Blowfish.new(key.encode('utf8'), Blowfish.MODE_ECB)
+    return c1.encrypt(pad(phrase))
+
+def doDecrypt(phrase):
+    c1  = Blowfish.new(key, Blowfish.MODE_ECB)
+    return unpad(c1.decrypt(phrase))
+
+
+def get_user(access_token):
+    jwt = from_jwt(access_token)
+    id = jwt.get('id', None)
+    return jwt, decode(id) if id else None, jwt.get('email',None)
+
+
+def allowed_file(filename):
+    filename, file_extension = os.path.splitext(filename)
+    return file_extension[1:] in ALLOWED_EXTENSIONS
+
+def upload_file(user_dir, _filename = None):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('hey niente', file=sys.stdout)
+            return False
+        file = request.files['file']
+        #TODO: convert image to png always
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('hey niente', file=sys.stdout)
+            return False
+
+        if file and allowed_file(file.filename):
+            filename =  _filename if _filename else secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], user_dir, filename)
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            file.save(path)
+            return url_for('static', filename=os.path.join('images',user_dir, filename), _external=True)
+    else:
+        print('method not was allowed')
+        return False
