08be21d176c7a0423310ca614b2e4163a33357b3
luigichougrad <gigibit92@gmail.com>
2020-03-01 17:38:39 +0100

    added initialization mood in employee statistics

--- website/views.py
+++ website/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'website/index.html', {})
+    lang = request.LANGUAGE_CODE.upper()
+    if lang == 'IT':
+        template = 'website/index_it.html'
+    elif lang == 'DE':
+        template = 'website/index_en.html'
+    else : 
+        template = 'website/index.html'
+    print(lang)
+    print(lang == 'IT')
+    print(template)
+    return render(request, template, {})
