140f7c13f93ca2f2cc8885409a0c0fac06989ede
luigichougrad <gigibit92@gmail.com>
2020-04-04 17:31:15 +0200

    removed ester egg

--- static/js/app.js
+++ static/js/app.js
@@ -5,62 +5,63 @@ function snackbar(text){
     setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
 
 }
-$(document).ready(function(){
-    $('giordy').hide()
+// $(document).ready(function(){
+    
+//     $('giordy').hide()
 
-    addEventListener("keydown", function(event) {
+//     addEventListener("keydown", function(event) {
 
         
-        if (event.keyCode == 86){
-            $('giordy').show()
-            $('div').hide()
-            setTimeout(
-                function(){
-                    $('giordy').css({
-                        'height':'5px',
-                        'width': '5px'
-                    })
-                    const interval = setInterval(()=> newPositionCount++>3 ? clearInterval(interval) : newPosition(), 1000)
-                }
-            ,500)
-            document.body.style.background = "violet";
-        }
-      });
-      addEventListener("keyup", function(event) {
-        if (event.keyCode == 86){
-            $('giordy').hide()
-            $('div:not(#spinit)').show()
+//         if (event.keyCode == 86){
+//             $('giordy').show()
+//             $('div').hide()
+//             setTimeout(
+//                 function(){
+//                     $('giordy').css({
+//                         'height':'5px',
+//                         'width': '5px'
+//                     })
+//                     const interval = setInterval(()=> newPositionCount++>3 ? clearInterval(interval) : newPosition(), 1000)
+//                 }
+//             ,500)
+//             document.body.style.background = "violet";
+//         }
+//       });
+//       addEventListener("keyup", function(event) {
+//         if (event.keyCode == 86){
+//             $('giordy').hide()
+//             $('div:not(#spinit)').show()
     
-            $('giordy').css({
-                'height':'100vh',
-                'width': '100vh',
-                'display': 'none'
-            })
-            document.body.style.background = "";
-        }
-      });
+//             $('giordy').css({
+//                 'height':'100vh',
+//                 'width': '100vh',
+//                 'display': 'none'
+//             })
+//             document.body.style.background = "";
+//         }
+//       });
 
-    function newPosition(){
-        $('giordy').css({
-            "left": Math.random() * window.innerWidth , 
-            "top": Math.random() * window.innerHeight
-        })
-    }
-    var newPositionCount = 0;
-    $('giordy').hover(function(){
-        $('giordy').css({
-                        'width': '100vh',
-                        'height': '100vh',
-                        'top': '0',
-                        'left': '0'
-                    })
-    })
-    $('giordy').mouseout(function(){
-            $('giordy').css({
-                            'width': '5px',
-                            'height': '5px',
-                        });
-           newPosition()
-    })
+//     function newPosition(){
+//         $('giordy').css({
+//             "left": Math.random() * window.innerWidth , 
+//             "top": Math.random() * window.innerHeight
+//         })
+//     }
+//     var newPositionCount = 0;
+//     $('giordy').hover(function(){
+//         $('giordy').css({
+//                         'width': '100vh',
+//                         'height': '100vh',
+//                         'top': '0',
+//                         'left': '0'
+//                     })
+//     })
+//     $('giordy').mouseout(function(){
+//             $('giordy').css({
+//                             'width': '5px',
+//                             'height': '5px',
+//                         });
+//            newPosition()
+//     })
 
-})
+// })
